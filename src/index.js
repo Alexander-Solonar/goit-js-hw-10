598bdea774c3fec2f23e814f19b624d0188ae6e1
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2023-02-27 22:07:11 +0200

    create fetchContries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
+const userInput = document.querySelector('#search-box');
 const DEBOUNCE_DELAY = 300;
+
+userInput.addEventListener('input', debounce(inputCountry, DEBOUNCE_DELAY));
+
+function inputCountry(event) {
+  const name = event.target.value.trim();
+  if (name === '') {
+    return;
+  }
+  fetchCountries(name);
+}
