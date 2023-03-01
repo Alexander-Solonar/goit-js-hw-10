c5458c83ba2114370fe54ac675add1034a4e7cf2
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2023-03-01 23:11:51 +0200

    fix hw-8

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,53 +1,5 @@
-import { notiflixInfo } from './index';
-import Notiflix from 'notiflix';
-
-const divCard = document.querySelector('.country-info');
-
 export function fetchCountries(name) {
-  fetch(
+  return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(respons => {
-      if (!respons.ok) {
-        throw new Error('Oops, there is no country with that name');
-      }
-      return respons.json();
-    })
-    .then(resp => {
-      if (resp.length > 10) {
-        notiflixInfo();
-      } else if (resp.length === 1) {
-        ulList.innerHTML = '';
-        marcurCardInfo(resp);
-      } else if (resp.length > 1 && resp.length <= 10) {
-        divCard.innerHTML = '';
-        foo(resp);
-      }
-    })
-    .catch(error => {
-      Notiflix.Notify.failure(`${error}`);
-    });
-}
-
-const ulList = document.querySelector('.country-list');
-
-function marcurCardInfo(info) {
-  return (divCard.innerHTML = `<div>
-  <img src="${info[0].flags.svg}" alt="" width='40'>
-  <h1>${info[0].name.official}</h1>
-  <p><h2>Capital:</h2><span>${info[0].capital.join()}</span></p>
-  <p><h2>Population:</h2><span>${info[0].population}</span></p>
-  <p><h2>Languages:</h2><span>${Object.values(info[0].languages).join(
-    ' '
-  )}</span></p>
-</div>`);
-}
-
-function foo(el) {
-  ulList.innerHTML = el.reduce((total, e) => {
-    return (
-      total +
-      `<li><img src="${e.flags.svg}" alt="" width='40'> <h2>${e.name.official}</h2></li>`
-    );
-  }, '');
+  );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
 
 const divCard = document.querySelector('.country-info');
+const ulList = document.querySelector('.country-list');
 const userInput = document.querySelector('#search-box');
 const DEBOUNCE_DELAY = 300;
 
@@ -13,13 +14,73 @@ function inputCountry(event) {
   const name = event.target.value.trim();
   if (name === '') {
     divCard.innerHTML = '';
+    ulList.innerHTML = '';
     return;
   }
-  fetchCountries(name);
+
+  fetchCountries(name)
+    .then(respons => {
+      if (!respons.ok) {
+        throw new Error();
+      }
+      return respons.json();
+    })
+    .then(checkValidity)
+    .catch(() => {
+      divCard.innerHTML = '';
+      ulList.innerHTML = '';
+      notiflixFailure();
+    });
 }
 
-export function notiflixInfo() {
+function notiflixInfo() {
   Notiflix.Notify.info(
     'Too many matches found. Please enter a more specific name.'
   );
+}
+
+function notiflixFailure() {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
+
+function createMarkupCard(info) {
+  const {
+    flags: { svg },
+    name: { official },
+    capital,
+    population,
+    languages,
+  } = info[0];
+
+  const language = Object.values(languages);
+
+  return (divCard.innerHTML = `
+  <div class ="country-name"><img src="${svg}" alt="Flag ${official}" width='40' height='30'>
+  <h1>${official}</h1></div>
+  <ul class="list-info">
+  <li class="item-info">Capital:<span class="span-info">${capital}</span></li>
+  <li class="item-info">Population:<span class="span-info">${population}</span></li>
+  <li class="item-info">Languages: <span class="span-info">${language}</span></li>
+</ul>`);
+}
+
+function createMarkupListCountry(info) {
+  ulList.innerHTML = info.reduce((html, { flags, name }) => {
+    return (
+      html +
+      `<li class=country-item><img src="${flags.svg}" alt="" width='50' height='40';> <h2>${name.common}</h2></li>`
+    );
+  }, '');
+}
+
+function checkValidity(respon) {
+  if (respon.length > 10) {
+    notiflixInfo();
+  } else if (respon.length === 1) {
+    ulList.innerHTML = '';
+    createMarkupCard(respon);
+  } else {
+    divCard.innerHTML = '';
+    createMarkupListCountry(respon);
+  }
 }
